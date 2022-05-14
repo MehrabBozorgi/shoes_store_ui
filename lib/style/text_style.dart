4d399beace1cae7519f60f91e4457cb876b819af
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2022-05-14 08:47:54 +0430

    shoes_store_ui

---
+++ lib/style/text_style.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+import 'colors.dart';
+
+const bigTitleBlackStyle = TextStyle(
+  color: primaryColor,
+  fontWeight: FontWeight.bold,
+  fontSize: 28,
+  wordSpacing: 2,
+);
+const bigTitleBlackStyle2 = TextStyle(
+  color: primaryColor,
+  fontWeight: FontWeight.bold,
+  fontSize: 24,
+  wordSpacing: 2,
+);
+const bigTitleBlackStyle3 = TextStyle(
+  color: primaryColor,
+  fontWeight: FontWeight.bold,
+  fontSize: 20,
+);
+const bigTitleWhiteStyle = TextStyle(
+  color: Colors.white,
+  fontWeight: FontWeight.bold,
+  fontSize: 26,
+);
+final bodyGreyStyle = TextStyle(
+  color: Colors.grey[400],
+  fontWeight: FontWeight.bold,
+  fontSize: 16,
+);
+final descGreyStyle = TextStyle(
+  color: Colors.grey[400],
+  fontWeight: FontWeight.bold,
+  fontSize: 14,
+  wordSpacing: 1,
+  height: 2,
+);
+const bodyBlackStyle = TextStyle(
+  color: primaryColor,
+  fontWeight: FontWeight.bold,
+  fontSize: 16,
+);
+const bodyWhiteStyle = TextStyle(
+  color: Colors.white,
+  fontWeight: FontWeight.bold,
+  fontSize: 16,
+);
